d7faed0af52bb0552414f3f1a5fb6a660713d674
Eli Arbel <aelij@arbel.net>
2018-03-18 19:00:25 +0200

    Add tests from AvalonEdit

---
+++ AvaloniaEdit.Tests/AvaloniaMocks/MockWindowingPlatform.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Avalonia.Platform;
+using Moq;
+
+namespace AvaloniaEdit.AvaloniaMocks
+{
+    public class MockWindowingPlatform : IWindowingPlatform
+    {
+        private readonly Func<IWindowImpl> _windowImpl;
+        private readonly Func<IPopupImpl> _popupImpl;
+
+        public MockWindowingPlatform(Func<IWindowImpl> windowImpl = null, Func<IPopupImpl> popupImpl = null)
+        {
+            _windowImpl = windowImpl;
+            _popupImpl = popupImpl;
+        }
+
+        public IWindowImpl CreateWindow()
+        {
+            return _windowImpl?.Invoke() ?? Mock.Of<IWindowImpl>(x => x.Scaling == 1);
+        }
+
+        public IEmbeddableWindowImpl CreateEmbeddableWindow()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IPopupImpl CreatePopup() => _popupImpl?.Invoke() ?? Mock.Of<IPopupImpl>(x => x.Scaling == 1);
+    }
+}
--- src/AvaloniaEdit/XmlnsAttributes.cs
+++ src/AvaloniaEdit/XmlnsAttributes.cs
@@ -1,7 +1,10 @@
-﻿using Avalonia.Metadata;
+﻿using System.Runtime.CompilerServices;
+using Avalonia.Metadata;
 
 [assembly: XmlnsDefinition("https://github.com/avaloniaui/avaloniaedit", "ICSharpCode.AvalonEdit")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui/avaloniaedit", "ICSharpCode.AvalonEdit.Editing")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui/avaloniaedit", "ICSharpCode.AvalonEdit.Rendering")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui/avaloniaedit", "ICSharpCode.AvalonEdit.Highlighting")]
 [assembly: XmlnsDefinition("https://github.com/avaloniaui/avaloniaedit", "ICSharpCode.AvalonEdit.Search")]
+
+[assembly: InternalsVisibleTo("AvaloniaEdit.Tests")]
