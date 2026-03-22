412cd7a9d406352c7b66577f9c4a74037fdc1fec
Dave Black <656118+udlose@users.noreply.github.com>
2026-03-22 14:39:35 -0500

    Replace Empty.Array class with built-in .NET Array.Empty<T>()

--- src/AvaloniaEdit/Utils/IFreezable.cs
+++ src/AvaloniaEdit/Utils/IFreezable.cs
@@ -24,94 +24,100 @@ using System.Linq;
 namespace AvaloniaEdit.Utils
 {
     internal interface IFreezable
-	{
-		/// <summary>
-		/// Gets if this instance is frozen. Frozen instances are immutable and thus thread-safe.
-		/// </summary>
-		bool IsFrozen { get; }
-		
-		/// <summary>
-		/// Freezes this instance.
-		/// </summary>
-		void Freeze();
-	}    
+    {
+        /// <summary>
+        /// Gets if this instance is frozen. Frozen instances are immutable and thus thread-safe.
+        /// </summary>
+        bool IsFrozen { get; }
+
+        /// <summary>
+        /// Freezes this instance.
+        /// </summary>
+        void Freeze();
+    }
 
     internal static class FreezableHelper
-	{
-		public static void ThrowIfFrozen(IFreezable freezable)
-		{
-			if (freezable.IsFrozen)
-				throw new InvalidOperationException("Cannot mutate frozen " + freezable.GetType().Name);
-		}
-		
-		public static IList<T> FreezeListAndElements<T>(IList<T> list)
-		{
-			if (list != null) {
-				foreach (var item in list)
-					Freeze(item);
-			}
-			return FreezeList(list);
-		}
-		
-		public static IList<T> FreezeList<T>(IList<T> list)
-		{
-			if (list == null || list.Count == 0)
-				return Empty<T>.Array;
-			if (list.IsReadOnly) {
-				// If the list is already read-only, return it directly.
-				// This is important, otherwise we might undo the effects of interning.
-				return list;
-			} else {
-				return new ReadOnlyCollection<T>(list.ToArray());
-			}
-		}
-		
-		public static void Freeze(object item)
-		{
-			var f = item as IFreezable;
-		    f?.Freeze();
-		}
-		
-		public static T FreezeAndReturn<T>(T item) where T : IFreezable
-		{
-			item.Freeze();
-			return item;
-		}
-		
-		/// <summary>
-		/// If the item is not frozen, this method creates and returns a frozen clone.
-		/// If the item is already frozen, it is returned without creating a clone.
-		/// </summary>
-		public static T GetFrozenClone<T>(T item) where T : IFreezable, ICloneable
-		{
-			if (!item.IsFrozen) {
-				item = (T)item.Clone();
-				item.Freeze();
-			}
-			return item;
-		}
-	}
+    {
+        public static void ThrowIfFrozen(IFreezable freezable)
+        {
+            if (freezable.IsFrozen)
+                throw new InvalidOperationException("Cannot mutate frozen " + freezable.GetType().Name);
+        }
+
+        public static IList<T> FreezeListAndElements<T>(IList<T> list)
+        {
+            if (list != null)
+            {
+                foreach (var item in list)
+                    Freeze(item);
+            }
+            return FreezeList(list);
+        }
+
+        public static IList<T> FreezeList<T>(IList<T> list)
+        {
+            if (list == null || list.Count == 0)
+                return Array.Empty<T>();
+            if (list.IsReadOnly)
+            {
+                // If the list is already read-only, return it directly.
+                // This is important, otherwise we might undo the effects of interning.
+                return list;
+            }
+            else
+            {
+                return new ReadOnlyCollection<T>(list.ToArray());
+            }
+        }
+
+        public static void Freeze(object item)
+        {
+            var f = item as IFreezable;
+            f?.Freeze();
+        }
+
+        public static T FreezeAndReturn<T>(T item) where T : IFreezable
+        {
+            item.Freeze();
+            return item;
+        }
+
+        /// <summary>
+        /// If the item is not frozen, this method creates and returns a frozen clone.
+        /// If the item is already frozen, it is returned without creating a clone.
+        /// </summary>
+        public static T GetFrozenClone<T>(T item) where T : IFreezable, ICloneable
+        {
+            if (!item.IsFrozen)
+            {
+                item = (T)item.Clone();
+                item.Freeze();
+            }
+            return item;
+        }
+    }
 
     internal abstract class AbstractFreezable : IFreezable
-	{
-	    /// <summary>
-		/// Gets if this instance is frozen. Frozen instances are immutable and thus thread-safe.
-		/// </summary>
-		public bool IsFrozen { get; private set; }
+    {
+        /// <summary>
+        /// Gets if this instance is frozen. Frozen instances are immutable and thus thread-safe.
+        /// </summary>
+        public bool IsFrozen { get; private set; }
 
-	    /// <summary>
-		/// Freezes this instance.
-		/// </summary>
-		public void Freeze()
-		{
-			if (!IsFrozen) {
-				FreezeInternal();
-				IsFrozen = true;
-			}
-		}
-		
-		protected virtual void FreezeInternal()
-		{
-		}
-	}
+        /// <summary>
+        /// Freezes this instance.
+        /// </summary>
+        public void Freeze()
+        {
+            if (!IsFrozen)
+            {
+                FreezeInternal();
+                IsFrozen = true;
+            }
+        }
+
+        protected virtual void FreezeInternal()
+        {
+        }
+    }
 }
